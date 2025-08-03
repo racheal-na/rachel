52f26a3523a55a7d9ef3cebc53ad24581159b9b7
rachel <rahelnaba716@gmail.com>
2025-08-03 06:49:54 -0700

    while and for

---
+++ switch.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int day=2;
+	switch(day){
+	
+	case 1:
+		cout<<"Sunday"<<endl;
+		break;
+	case 2:
+		cout<<"Monday"<<endl;
+		break;
+	case 3:
+	    cout<<"Tuesday"<<endl;
+		break;
+	case 4:
+	  cout<<"Wednesday"<<endl;
+	 break;
+	default:
+	cout<<"Other day"<<endl;	
+}
+return 0;
+}
---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter number: "<<endl;
+	cin>>n;
+
+	while(n<=5){
+			cout<<" "<<n<<endl;
+		n++;
+		}	
+	
+	cout<<"Fire"<<endl;
+	return 0;
+}
