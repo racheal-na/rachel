01dde8322c94d4ce1bde9b67783826f696eea4ec
rachel <rahelnaba716@gmail.com>
2025-08-03 14:25:58 -0700

    sage

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# sage
-This is my first Git project
-
-Now I edit README.md
-#EDITED#
+# sage git
---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int marks[5];
+	cout<<"Enter 5 marks: ";
+	for(int i=0; i<5; i++){
+	
+	cin>>marks[i];
+}
+
+cout<<"You entered: ";
+for(int i=0; i<5; i++){
+	cout<<marks[i]<<" ";
+}
+return 0;
+}
